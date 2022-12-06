4dbe564dc7a7c84721acd74df23ed8213ebfc3ef
Ivan Minakov <i.minakov786@gmail.com>
2022-12-06 07:24:44 -0500

    task1 done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿void CountNumber()
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    Random rnd = new Random();
+    int count = 0;
+    int[] array = new int[number];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+        Console.Write($"{array[i]} ");
+
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine();
+    System.Console.Write($"Количество четных чисел в массиве - {count}");
+}
+
+System.Console.WriteLine("Введите длину массива");
+CountNumber();
